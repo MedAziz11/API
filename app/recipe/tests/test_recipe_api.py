dbf9ab18c70dfd147cd98d36afb17c60cbfacd98
MedAziz11 <58366672+MedAziz11@users.noreply.github.com>
2020-07-19 12:42:11 +0100

    adding image field

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -156,3 +156,50 @@ class AuthRecipeTests(TestCase):
         self.assertEqual(ingredients.count(), 2)
         self.assertIn(ingredient1, ingredients)
         self.assertIn(ingredient2, ingredients)
+
+    def test_partial_update_recipe(self):
+        """testing a partial update for the recipe"""
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        new_tag = sample_tag(user=self.user, name="tasty")
+        payload = {
+            'title': 'threesome pasta',
+            'tags': [new_tag.id]
+        }
+        url = detail_url(recipe.id)
+        self.client.patch(url, payload)
+        recipe.refresh_from_db()
+        self.assertEqual(payload['title'], recipe.title)
+        tags = recipe.tags.all()
+        self.assertEqual(tags.count(), 1)
+        self.assertIn(new_tag, tags)
+
+    def test_full_update_recipe(self):
+        """test a full update for the recipe"""
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        recipe.ingredients.add(sample_ingredient(user=self.user))
+        new_tag = sample_tag(user=self.user, name="tasty")
+        new_ingredient = sample_ingredient(user=self.user, name="Hrissa")
+        payload = {
+            'title': 'chicken nuggets',
+            'price': 12,
+            'time_minutes': 30,
+            'tags': [new_tag.id],
+            'ingredients': [new_ingredient.id]
+        }
+        url = detail_url(recipe.id)
+        self.client.put(url, payload)
+        recipe.refresh_from_db()
+
+        self.assertEqual(payload['title'], recipe.title)
+        self.assertEqual(payload['price'], recipe.price)
+        self.assertEqual(payload['time_minutes'], recipe.time_minutes)
+
+        tags = recipe.tags.all()
+        self.assertEqual(tags.count(), 1)
+        self.assertIn(new_tag, tags)
+
+        ingredients = recipe.ingredients.all()
+        self.assertEqual(ingredients.count(), 1)
+        self.assertIn(new_ingredient, ingredients)
